cdc8d5852a5440ddb02fe5062a44c3bf2a1e46d4
Johan Hidding <j.hidding@esciencecenter.nl>
2017-02-21 22:28:26 +0100

    python venv indicator

---
+++ bin/python-venv-bar.sh
@@ -0,0 +1,14 @@
+function virtualenv_info(){
+    # Get Virtual Env
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        # Strip out the path and just leave the env name
+        venv="${VIRTUAL_ENV##*/}"
+    else
+        # In case you don't have one activated
+        venv=''
+    fi
+    [[ -n "$venv" ]] && echo "🐍:$venv"
+}
+
+virtualenv_info
+
