38e1b4b9467c0368e452aac1e0d758a0da0ba6a0
Johan Hidding <j.hidding@esciencecenter.nl>
2022-05-31 13:48:50 +0200

    make git and python venv info shorter

--- bin/git-bar.sh
+++ bin/git-bar.sh
@@ -2,10 +2,11 @@
 
 if git rev-parse 2> /dev/null
 then
-        branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null) || branch="~"
-        repo=$(basename $(git rev-parse --show-toplevel 2> /dev/null))
-        git diff-index --quiet HEAD 2> /dev/null && mod="" || mod=":M"
-        echo ":${repo}:${branch}${mod}"
+        # branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null) || branch="~"
+        branch=$(git symbolic-ref --short HEAD 2> /dev/null) || branch="~"
+        # repo=$(basename $(git rev-parse --show-toplevel 2> /dev/null))
+        git diff-index --quiet --cached HEAD 2> /dev/null && mod="" || mod=":M"
+        echo ":${branch}${mod}"
         exit 0
 else
         exit 1
--- bin/python-venv-bar.sh
+++ bin/python-venv-bar.sh
@@ -3,6 +3,9 @@ function virtualenv_info(){
     if [[ -n "$VIRTUAL_ENV" ]]; then
         # Strip out the path and just leave the env name
         venv="${VIRTUAL_ENV##*/}"
+        if [[ "$venv" =~ ([^-]+)-[^-]+-py([0-9\.]+) ]]; then
+                venv="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
+        fi
     elif [[ -n "$CONDA_DEFAULT_ENV" ]]; then
 	# For conda environments
         venv="${CONDA_DEFAULT_ENV##*/}"
